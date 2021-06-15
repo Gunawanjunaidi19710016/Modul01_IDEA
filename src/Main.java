20a03ad8596aaa79f0b2781e6e58a385be50e4ee
Rskiw <riskinurliana03@gmail.com>
2021-06-15 11:27:36 +0800

    Operasi Tipe String

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         String namaDepan = "Riski";
@@ -13,5 +15,21 @@ public class Main {
         char[] uniskaChar = {'U', 'N', 'I', 'S', 'K', 'A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan + " " +
+                namaDepan + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.replace("Ris", "Nur"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for (String nama : namaArray){
+            System.out.println(nama);
+        }
     }
 }
